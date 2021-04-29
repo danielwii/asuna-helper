e36f6635a1660b3fe090fadb9c7e44d91d7cc77b
Daniel Wei <danielwii0326@gmail.com>
2021-04-29 22:02:27 +0800

    feat: add downloader

---
+++ src/downloader.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosResponse } from 'axios';
+import { Promise } from 'bluebird';
+import * as fs from 'fs-extra';
+import path, { join } from 'path';
+
+import { handleAxiosResponseError } from './axios';
+import { LoggerFactory } from './logger';
+import { r } from './serializer';
+
+const logger = LoggerFactory.getLogger('Downloader');
+
+export async function download(url: string, to: string): Promise<AxiosResponse> {
+  fs.ensureDirSync(path.dirname(to));
+  const dir = path.resolve(to);
+
+  const writer = fs.createWriteStream(dir);
+
+  const response = await axios({ url, method: 'GET', responseType: 'stream', timeout: 60000 });
+
+  response.data.pipe(writer);
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+}
+
+export function fetchFile(url: string, to: string): Promise<string | AxiosResponse> {
+  // const host = AppConfigObject.load().masterAddress;
+  const host = process.env.MASTER_ADDRESS;
+  let endpoint = url;
+  if (!url.startsWith('http')) {
+    const fixedPath = join('/', url).replace(/^\/+/, '/');
+    // `${host}${fixedPath}?internal=1`
+    endpoint = new URL(`${fixedPath}?internal=1`, host).href;
+  }
+  logger.log(`fetch file: ${r({ endpoint, url, to })}`);
+  return download(endpoint, to).catch((error) => handleAxiosResponseError(endpoint, error));
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,26 @@
+import * as JSON5 from 'json5';
+
 export const isProductionEnv = process.env.NODE_ENV === 'production';
+
+export const withP = <P, R>(parameter: P, fn: (p: P) => R) => fn(parameter);
+export const withP2 = <P1, P2, R>(parameter1: P1, parameter2: P2, fn: (p1: P1, p2: P2) => R) =>
+  fn(parameter1, parameter2);
+export const withP3 = <P1, P2, P3, R>(
+  parameter1: P1,
+  parameter2: P2,
+  parameter3: P3,
+  fn: (p1: P1, p2: P2, p3: P3) => R,
+) => fn(parameter1, parameter2, parameter3);
+export const fnWithP2 = <P1, P2, R>(parameter1: P1, parameter2: P2) => (fn: (p1: P1, p2: P2) => R): R =>
+  fn(parameter1, parameter2);
+export const fnWithP3 = <P1, P2, P3, R>(parameter1: P1, parameter2: P2, parameter3: P3) => (
+  fn: (p1: P1, p2: P2, p3: P3) => R,
+): R => fn(parameter1, parameter2, parameter3);
+
+export function parseJSONIfCould(value?: string): any {
+  try {
+    return JSON5.parse(value ?? '');
+    // eslint-disable-next-line no-empty
+  } catch {}
+  return value;
+}
