3de6d3fe44cde1d11be8f8a5f51adf6f8f8c610d
Daniel Wei <danielwii0326@gmail.com>
2021-08-21 04:38:03 +0800

    refactor: add class types

--- src/interface.ts
+++ src/interface.ts
@@ -1,1 +1,3 @@
-export declare type ClassType<T> = new (...args: any[]) => T;
+export type ClassType<T> = new (...args: any[]) => T;
+export type Constructor = new (...args: any[]) => {};
+export type ConstrainedConstructor<T = {}> = new (...args: any[]) => T;
