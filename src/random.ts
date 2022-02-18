383212791136744bc3c819afdc54eb9813e76725
Daniel Wei <danielwii0326@gmail.com>
2022-02-18 13:52:21 +0800

    fix: remove deprecated shortid

--- src/random.ts
+++ src/random.ts
@@ -1,9 +1,9 @@
-import shortid from 'shortid';
+import { nanoid } from 'nanoid';
 
 export function random(length = 9): string {
   return new Array(Math.ceil(length / 9))
     .fill(0)
-    .map(() => shortid.generate())
+    .map(() => nanoid())
     .join('')
     .slice(0, length);
 }
